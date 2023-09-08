e69aa403abd7a6fde13942abdb7d0dccf69f68b9
TAKAHASHIYUTOGIT <134475821+TAKAHASHIYUTOGIT@users.noreply.github.com>
2023-09-08 23:20:36 +0900

    ボールスクリプト作成

---
+++ Assets/BallScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{   
+    public bool Ball1 = false;
+    public bool Ball2 = false;
+    public bool Ball3 = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text scoreText;
+    private int score = 0;
+    void Start()
+    {
+        score = 0;
+    }
+    void Update()
+    {
+        
+    }
+}
