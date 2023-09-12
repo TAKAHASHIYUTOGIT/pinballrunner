e3210cf256560b58ae609acafe2e0b92c6e7a219
TAKAHASHIYUTOGIT <134475821+TAKAHASHIYUTOGIT@users.noreply.github.com>
2023-09-12 21:01:02 +0900

    障害物追加　ジェネレータースクリプト作成

---
+++ Assets/BallGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    float span = 1f;
+    float delta = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
